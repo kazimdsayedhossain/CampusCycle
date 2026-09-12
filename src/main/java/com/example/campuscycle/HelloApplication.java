d50243076d2972601689b7e353f27ec53153690e
Kazi Sayed Hossain <kazimdsayedhossain@outloo.com>
2026-09-12 14:16:40 +0600

    Title and Image added

--- src/main/java/com/example/campuscycle/HelloApplication.java
+++ src/main/java/com/example/campuscycle/HelloApplication.java
@@ -1,19 +1,30 @@
 package com.example.campuscycle;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage firstStage) {
+        // 1. Initialize the StackPane root node
+        StackPane root = new StackPane();
+
+        // 2. Initialize the Scene with the root node and window dimensions (width, height)
+        Scene firstStageScene = new Scene(root, 600, 400);
+
+        // 3. Set up and show the stage (window)
+        firstStage.setTitle("CampuCycle");
+        Image title_img = new Image(getClass().getResource("/com/example/campuscycle/title_image.png").toExternalForm());
+        firstStage.getIcons().add(title_img);
+
+
+
+        firstStage.setScene(firstStageScene);
+        firstStage.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.campuscycle {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.campuscycle to javafx.fxml;
